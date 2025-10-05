5fed2431179f5bff88d56ece17a885e79f7b1f9c
Aivaras-P <s054117@ad.viko.lt>
2025-10-05 20:32:40 +0300

    Create recipes.js

---
+++ app/data/recipes.js
@@ -0,0 +1,38 @@
+export const recipes = {
+  soups: [
+    {
+      id: "soup1",
+      title: "Moliūgų sriuba",
+      ingredients: [
+        "500g moliūgo",
+        "1 svogūnas",
+        "1l daržovių sultinio",
+        "Druska, pipirai"
+      ],
+      instructions: "Moliūgą supjaustyk, virk sultinyje 20 min, sutrink blenderiu.",
+    },
+  ],
+  desserts: [
+    {
+      id: "dessert1",
+      title: "Avižiniai sausainiai",
+      ingredients: [
+        "2 puodeliai avižų",
+        "1 kiaušinis",
+        "2 šaukštai medaus"
+      ],
+      instructions: "Sumaišyk ingredientus, kepk 180°C 15 minučių.",
+    },
+  ],
+  main: [
+    {
+      id: "main1",
+      title: "Kepta vištienos krūtinėlės filė",
+      ingredients: [
+        "1 vištienos krūtinėlė",
+        "Druska, pipirai, alyvuogių aliejus"
+      ],
+      instructions: "Paskanink, kepk keptuvėje 8-10 minučių iš abiejų pusių.",
+    },
+  ]
+};
